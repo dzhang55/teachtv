a49cb87a5f0db07532475c19a73670a487aaa9ad
karinna <karinnaloo@pacbell.net>
2018-04-09 15:26:13 -0400

    firebase login live

--- app/src/main/java/com/karinnaloo/teachtv/NewRegisterActivity.java
+++ app/src/main/java/com/karinnaloo/teachtv/NewRegisterActivity.java
@@ -10,59 +10,46 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
-public class NewLoginActivity extends Activity {
-
+public class NewRegisterActivity extends Activity{
     private EditText inputEmail, inputPassword;
-    private FirebaseAuth auth;
+    private Button btnLinkLogin, btnSignUp, btnResetPassword;
     private ProgressBar progressBar;
-    private Button btnLogin;
-    private Button btnLinkRegister;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        auth = FirebaseAuth.getInstance();
-
-        if (auth.getCurrentUser() != null) {
-            startActivity(new Intent(NewLoginActivity.this, MainActivity.class));
-            finish();
-        }
-
-        // set the view now
-        setContentView(R.layout.teach_login);
-
-        inputEmail = (EditText) findViewById(R.id.email);
-        inputPassword = (EditText) findViewById(R.id.password);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        btnLogin = (Button) findViewById(R.id.btn_login);
-        btnLinkRegister = (Button) findViewById(R.id.register_link_button);
-
-        btnLinkRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(NewLoginActivity.this, NewRegisterActivity.class));
-                finish();
-            }
-        });
+        setContentView(R.layout.teach_register);
 
         //Get Firebase auth instance
         auth = FirebaseAuth.getInstance();
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        btnSignUp = (Button) findViewById(R.id.sign_up_button);
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        btnLinkLogin = (Button) findViewById(R.id.login_link_button);
+
+        btnLinkLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = inputEmail.getText().toString();
-                final String password = inputPassword.getText().toString();
+                startActivity(new Intent(NewRegisterActivity.this, NewLoginActivity.class));
+                finish();
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
 
                 if (TextUtils.isEmpty(email)) {
                     Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
@@ -74,29 +61,29 @@ public class NewLoginActivity extends Activity {
                     return;
                 }
 
-                progressBar.setVisibility(View.VISIBLE);
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
-                //authenticate user
-                auth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(NewLoginActivity.this, new OnCompleteListener<AuthResult>() {
+                progressBar.setVisibility(View.VISIBLE);
+                //create user
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(NewRegisterActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(NewRegisterActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.GONE);
                                 // If sign in fails, display a message to the user. If sign in succeeds
                                 // the auth state listener will be notified and logic to handle the
                                 // signed in user can be handled in the listener.
-                                progressBar.setVisibility(View.GONE);
                                 if (!task.isSuccessful()) {
-                                    // there was an error
-                                    if (password.length() < 6) {
-                                        inputPassword.setError("Password must be at least 6 characters.");
-                                    } else {
-                                        Toast.makeText(NewLoginActivity.this, "Invalid login.", Toast.LENGTH_LONG).show();
-                                    }
+                                    Toast.makeText(NewRegisterActivity.this, "Authentication failed." + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
                                 } else {
-                                    Toast.makeText(NewLoginActivity.this, "USER LOGGED IN", Toast.LENGTH_LONG).show();
-                                    // TODO: EVENTUAL LOG INTO HOME
-//                                    Intent intent = new Intent(NewLoginActivity.this, MainActivity.class);
-//                                    startActivity(intent);
+                                    Toast.makeText(NewRegisterActivity.this, "USER LOGGED IN", Toast.LENGTH_LONG).show();
+                                    // TODO: EVENTUAL LOGIN
+//                                    startActivity(new Intent(NewRegisterActivity.this, Home.class));
 //                                    finish();
                                 }
                             }
@@ -104,4 +91,11 @@ public class NewLoginActivity extends Activity {
             }
         });
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
+    }
+
 }
